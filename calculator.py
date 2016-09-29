820881ab3a24a220236bd6323b525d2f777d0198
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:43:32 -0700

    Dictionary form. Draft

--- calculator.py
+++ calculator.py
@@ -16,36 +16,20 @@ while True:
 		if len(list_input) == 3:
 			argument_1, argument_2 = int(list_input[1]), int(list_input[2])
 		elif len(list_input) == 2:
-			argument_1 = int(list_input[1])
+			argument_1, argument_2 = int(list_input[1]), 5
 
-		if list_input[0] == "+":
-			result = add(argument_1, argument_2)
-			print result
+		maths = {
+			"+": add(argument_1, argument_2), 
+			"-": subtract(argument_1, argument_2), 
+			"*": multiply(argument_1, argument_2), 
+			"/": divide(argument_1, argument_2), 
+			"square": square(argument_1), 
+			"cube": cube(argument_1), 
+			"pow": power(argument_1, argument_2), 
+			"mod": mod(argument_1, argument_2)}
 
-		elif list_input[0] == "-":
-			result = subtract(argument_1, argument_2)
-			print result
 
-		elif list_input[0] == "*":
-			result = multiply(argument_1, argument_2)
-			print result
-
-		elif list_input[0] == "/":
-			result = divide(argument_1, argument_2)
-			print result
-
-		elif list_input[0] == "square":
-			result = square(argument_1)
-			print result
-
-		elif list_input[0] == "cube":
-			result = cube(argument_1)
-			print result
-
-		elif list_input[0] == "pow":
-			result = power(argument_1, argument_2)
-			print result
-
-		elif list_input[0] == "mod":
-			result = mod(argument_1, argument_2)
-			print result
+		if list_input[0] in maths:
+			value = maths[list_input[0]]
+			print value
+		
