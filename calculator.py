bd084a09b8fd11638644033d9ee14c933c8e76c4
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 13:22:15 -0700

    Added error handling to input to only accept integers

--- calculator.py
+++ calculator.py
@@ -6,6 +6,23 @@ calculator program yourself in this file.
 """
 
 from arithmetic import *
+
+def calculate(operand, numbers):
+	maths = {
+		"+": add,
+		"-": subtract,
+		"*": multiply,
+		"/": divide,
+		"square": square,
+		"cube": cube,
+		"pow": power,
+		"mod": mod
+		}
+
+	fn_to_call = maths[operand]
+	print fn_to_call(numbers)
+
+
 while True:
 	user_input = raw_input(">")
 
@@ -15,20 +32,11 @@ while True:
 		list_input = user_input.split(" ")
 		numbers = []
 		for item in list_input[1:]:
-			numbers.append(int(item))
-
-		maths = {
-			"+": add,
-			"-": subtract,
-			"*": multiply,
-			"/": divide,
-			"square": square,
-			"cube": cube,
-			"pow": power,
-			"mod": mod
-			}
-
-		fn_to_call = maths[list_input[0]]
-
-		print fn_to_call(numbers)
+			for char in item:
+				if char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+					numbers.append(int(item))
+				else:
+					print "Your entry doesn't look like a number! This is a calculator! Please try again."
+					break
+		calculate(list_input[0], numbers)
 		
